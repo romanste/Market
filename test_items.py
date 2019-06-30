34d07c69d4c1ab26f34fbdd2d74ad5de98c512dd
romanste <52203644+romanste@users.noreply.github.com>
2019-06-30 23:13:16 +0300

    Delete test_items.py

--- test_items.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-def test_basket_button_existing(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    browser.find_element_by_class_name('btn-add-to-basket')
-    time.sleep(30)  # for language verification
